0ffdd08b92707f499326900ba052b35d90991cad
flaska99 <tmdalsehrwhs@naver.com>
2025-05-03 21:46:03 +0900

    echo server

---
+++ webproxy-lab/echo/echoclient.c
@@ -0,0 +1,28 @@
+#include "csapp.h"
+
+int main(int argc, char **argv)
+{
+    int clientfd; // 클라이언트 파일 디스크립터
+    char *host, *port, buf[MAXLINE];
+    rio_t rio;
+
+    if (argc != 3){
+        fprintf(stderr, "usage : %s <host> <port> \n", argv[0]);
+        exit(0);
+    }
+
+    host = argv[1];
+    port = argv[2];
+
+    clientfd = Open_clientfd(host, port);
+    Rio_readinitb(&rio, clientfd);
+
+    while(Fgets(buf, MAXLINE, stdin) != NULL) {
+        Rio_writen(clientfd, buf, strlen(buf));0
+        Rio_readlineb(&rio, buf, MAXLINE);
+        Fputs(buf, stdout);
+    }
+
+    Close(clientfd);
+    exit(0);
+}
